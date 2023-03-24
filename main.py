11c56bbf8525d7a4cb6ec0e7c4811e56dc77e10c
BeytullahAksoy <beytullah557@gmail.com>
2023-03-24 13:47:57 +0300

    add kafka connection template

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
+from kafka import KafkaProducer
 import time
 import requests
+import json
 
+producer = KafkaProducer(bootstrap_servers = ['34.132.165.75'],value_serializer=lambda x: json.dumps(x).encode('utf-8'))
 while True:
     api_url = "https://api.orhanaydogdu.com.tr/deprem/kandilli/live"
     response = requests.get(api_url, stream=True)
+    data=response.json()
     try :
-        for line in response.iter_lines():
-            if line:
-                print(line)
+        for line in data:
+                producer.send('depremTopic', value=line)
     except Exception as e:
         print("Error")
 
-    time.sleep(1)
+  #  time.sleep(1)
