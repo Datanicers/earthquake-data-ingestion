ad8f384769a071fc04f96646c07a622b7039a354
BeytullahAksoy <beytullah557@gmail.com>
2023-04-08 17:03:49 +0300

    fix producer

--- marketsales.py
+++ marketsales.py
@@ -2,13 +2,12 @@ import pandas as pd
 from kafka import KafkaProducer
 #from kafka import KafkaConsumer
 
-#producer = KafkaProducer(bootstrap_servers=['localhost:9092'])
-df = pd.read_csv("MarketSales.csv",low_memory=False)
+producer = KafkaProducer(bootstrap_servers=['localhost:9092'])
+df = pd.read_csv("/home/beytullah557/MarketSales_(1).csv",low_memory=False)
 df['all_data'] = df[df.columns[0:]].apply(
     lambda x: ','.join(x.dropna().astype(str)),
     axis=1
 )
-print(len(df['all_data']))
+
 for i in df['all_data']:
-     print(i.encode("utf-8"))
-     break
+    producer.send('marketsales', i.encode("utf-8"))
